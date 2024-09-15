a52760d6bd43b5c602b1b48e6fb979512eb2e689
trenblack <trenblack12@gmail.com>
2024-09-15 05:06:45 -0700

    added default value for secret

--- app/settings.py
+++ app/settings.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", default="secret")
 JWT_ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 REFRESH_TOKEN_EXPIRE_DAYS = 30
